da4b0208cff6ba8d03d6ded5205642cbb5ac0645
panymari <panymari84@gmail.com>
2022-08-16 18:35:28 +0300

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const todos = ref([]);
+const name = ref('');
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const inputContent = ref('');
+const inputCategory = ref(null);
+
+computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}));
+const addTodo = () => {
+  if (inputContent.value.trim() === '' || inputCategory.value === null) {
+    return
   }
-}
+
+  todos.value.push({
+    content: inputContent.value,
+    category: inputCategory.value,
+    done: false,
+    createdAt: new Date().getTime()
+  });
+};
+
+watch(todos, (newVal) => {
+  localStorage.setItem('todos', JSON.stringify(newVal));
+}, { deep: true });
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || '';
+  todos.value = JSON.parse(localStorage.getItem('todos')) || [];
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <main class="class">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>Create a todo</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+            type="text"
+            placeholder="e.g. make a video"
+            v-model="inputContent" />
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+                type="radio"
+                name="category"
+                value="businessman"
+                v-model="inputCategory" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+                type="radio"
+                name="category"
+                value="personal"
+                v-model="inputCategory" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo">
+      </form>
+    </section>
+
+    <section class="todo-list"></section>
+  </main>
+</template>
