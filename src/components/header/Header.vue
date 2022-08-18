8a70674c341469ae096679a9e26e4540d1eeace4
panymari <panymari84@gmail.com>
2022-08-18 18:38:51 +0300

    commit2

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -30,6 +30,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 @import './header.scss';
 </style>
--- src/store/modules/recommendations.js
+++ src/store/modules/recommendations.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     actions: {
-         fetchRecommendations({ commit, getters, dispatch }, limit = 3) {
+         fetchRecommendations({ commit, getters, dispatch }, limit = 5) {
             axios
                 .get('https://jsonplaceholder.typicode.com/todos?_limit=' + limit)
                 .then(response => {
